0cbd40a523e3af8467df92fb9c3e2f5cca04623a
Jonathan Haas <jonathan.haas.ger@gmail.com>
2023-02-06 12:17:48 +0100

    Save

--- notebooks/04-prepare-gittables.py
+++ notebooks/04-prepare-gittables.py
@@ -26,6 +26,10 @@ initialise_nltk()
 
 # %%
 DATA_DIR = pathlib.Path("/home/agemcipe/code/hpi_coursework/master_thesis/semanum/data/gittables")
+MODEL_ID = "gittables_full"
+LEAST_TARGET_COUNT = 100 # Should not have an effect on the results for full dataset
+FEATURES_FILE_NAME = f"{MODEL_ID}_features.csv"
+
 ont_file = DATA_DIR / "dbpedia_semantic_types_filtered_1000.csv"
 index_file = DATA_DIR / "mapping_column_name_semantic_type.csv"
 
@@ -36,29 +40,23 @@ index_df = pd.read_csv(index_file)
 index_df = index_df[index_df["dbpedia_semantic"].isin(ont_df["id"])]
 
 # %%
-_local_files = [
-"allegro_con_spirito_tables_licensed" , 
-"beats_per_minute_tables_licensed", 
-"solar_constant_tables_licensed", 
-"wartime_tables_licensed", 
-"attrition_rate_tables_licensed", 
-"radial_velocity_tables_licensed", 
-"speed_of_light_tables_licensed", 
-]
-
-_index_df = index_df[index_df["file_name"].str.startswith(tuple(_local_files))]
+_local_dirs = [d.name for d in  (DATA_DIR / "unzipped").glob("*")]
+if _local_dirs:
+    _index_df = index_df[index_df["file_name"].str.startswith(tuple(_local_dirs))]
+else:
+    print("No local dirs found")
+    raise FileNotFoundError()
 # %% 
-
 def _get_data_and_targets(row):
-    i = row[0]
-    if i % 100 == 0:
-        print(i)
-
     row = row[1]
     _fp = DATA_DIR / "unzipped" / row["file_name"]
-    _data = pd.read_parquet(_fp)[row["column_name"]]
-    if not _data.isnull().all():
-        return str(_data.values.tolist()), row["dbpedia_semantic"]
+    try:
+        _data = pd.read_parquet(_fp)[row["column_name"]]
+        if not _data.isnull().all():
+            return str(_data.values.tolist()), row["dbpedia_semantic"]
+    except Exception as e:
+        print(e)
+        return None
 
 def get_data_and_targets(index_df: pd.DataFrame, n: int = 1000):
     """Read data from parquet files and return data and targets.
@@ -77,7 +75,7 @@ def get_data_and_targets(index_df: pd.DataFrame, n: int = 1000):
     print("Total:", total)
 
     res = process_map(
-        _get_data_and_targets, it(), max_workers=4, total=total
+        _get_data_and_targets, it(), total=total
     )
 
     res = [r for r in res if r is not None]
@@ -85,7 +83,7 @@ def get_data_and_targets(index_df: pd.DataFrame, n: int = 1000):
     return list(data), list(targets)
 
 
-data, targets = get_data_and_targets(_index_df.reset_index(), n=3000)
+data, targets = get_data_and_targets(_index_df.reset_index())
 assert len(data) == len(targets)
 
 print(len(data), len(targets))
@@ -97,23 +95,15 @@ raw_targets = targets
 data = pd.Series(raw_data, name="values")
 targets = pd.Series(raw_targets, name="labels")
 
-_targets = targets.value_counts() > 15
-_targets = _targets[_targets].index.tolist()
-_targets = [
-    "http://dbpedia.org/ontology/temperature",
-    "http://dbpedia.org/ontology/speedLimit",
-    "http://dbpedia.org/ontology/number",
-    "http://dbpedia.org/ontology/boilingPoint",
-]
-idx = targets[targets.isin(_targets)].index
+targets_fil_count = targets.value_counts()[targets.value_counts() > LEAST_TARGET_COUNT].index
+
+idx = targets[targets.isin(targets_fil_count)].index
 targets = targets[idx]
 data = data[idx]
 
 # %% 
-feature_file_name = "../gittables_sample.csv"
+feature_file_name = f"../{FEATURES_FILE_NAME}"
 extract_features_to_csv(output_path=feature_file_name, parquet_values=data)
-# extract_features(output_filename=feature_file_name, data=data)
-# extract_features_to_csv(output_path=feature_file_name, parquet_values=data)
 
 # %% 
 feature_vectors = pd.read_csv(feature_file_name, dtype=np.float32)
@@ -124,6 +114,7 @@ X_train, X_validation, y_train, y_validation = train_test_split(feature_vectors,
 
 print(f"Train size: {len(X_train)}")
 print(f"Validation size: {len(X_validation)}")
+print(f"Distinct labels in train: {len(set(y_train))}")
 
 columns_with_na_values = X_train.columns[X_train.isna().any()]
 print("Columns with NA values:", columns_with_na_values)
@@ -133,23 +124,23 @@ print("Columns with NA values:", columns_with_na_values)
 train_columns_means = pd.DataFrame(X_train.mean()).transpose()
 
 # %%
+print("Imputing NA values with means...")
 X_train = X_train.fillna(train_columns_means.iloc[0])
 X_validation = X_validation.fillna(train_columns_means.iloc[0]) # is this right using train mean?
 
 # %%
-model_id = 'gittables_sample'
 start = datetime.now()
 print(f'Started at {start}')
 
 model = SherlockModel()
 # Model will be stored with ID `model_id`
-model.fit(X_train, y_train, X_validation, y_validation, model_id=model_id)
+model.fit(X_train, y_train, X_validation, y_validation, model_id=MODEL_ID)
 
 print('Trained and saved new model.')
 print(f'Finished at {datetime.now()}, took {datetime.now() - start} seconds')
 
-model.store_weights(model_id=model_id)
+model.store_weights(model_id=MODEL_ID)
 # %%
-t = model.predict(X_test, model_id=model_id)
-sum(t == y_test) / len(y_test)
+t = model.predict(X_test, model_id=MODEL_ID)
+print("Test Acc.", sum(t == y_test) / len(y_test))
 # %%
