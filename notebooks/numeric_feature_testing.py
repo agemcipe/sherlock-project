dca613a868d2b04bf789c5f639ba221410f5a06a
Jonathan Haas <jonathan.haas.ger@gmail.com>
2023-03-02 19:57:52 +0100

    Save

--- notebooks/numeric_feature_testing.py
+++ notebooks/numeric_feature_testing.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 DATA_DIR = pathlib.Path("/home/agemcipe/code/hpi_coursework/master_thesis/data/gittables")
 FP = DATA_DIR / "data.csv"
+
 output_path = DATA_DIR / "test_output.parquet"
 data = pd.read_csv(FP, nrows=10)["values"].astype(str)
 
